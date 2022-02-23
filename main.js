a5c8864e0b07e490ada9cbfb30dfa6b0d2b45fef
qazedc159 <100271104+qazedc159@users.noreply.github.com>
2022-02-23 15:47:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(function(){
+  /* Back top */
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+})
+
